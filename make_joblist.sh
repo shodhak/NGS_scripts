05e42641070f888734ef1f27583069fd2d8ed9eb
Shreyas Joshi <ssj262@gmail.com>
2018-09-11 13:08:48 -0400

    makes jobfiles limited to 300 lines for 300 CPU limit

--- make_joblist.sh
+++ make_joblist.sh
@@ -1,7 +1,21 @@
 #!/bin/bash
 
-for dir in /home/sj577/project/hun_seqs/*/
+for dir in /home/sj577/project/all_sequences/*/
 	do
-		echo $dir
-		echo "pipeline.sh $dir/Unaligned/*R1_001.fastq.gz $dir/Unaligned/*R2_001.fastq.gz" >> jobfile.txt
+		#echo $dir
+		echo "sh pipeline.sh $dir/Unaligned/*R1_001.fastq.gz $dir/Unaligned/*R2_001.fastq.gz" >> jobfile.txt
 	done
+
+#In HPC 300 cores are assigned for a single run.
+#Make one job file with the first 300 lines
+
+head -300 jobfile.txt > jobfile1.txt
+
+#Count total number of lines in the jobfile
+nlines=$(wc -l < jobfile.txt)
+echo $nlines
+#Number of lines left after the first 300
+tlines=$((nlines-300))
+echo $tlines
+#Copy those lines into a new job file
+tail -$tlines jobfile.txt > jobfile2.txt
