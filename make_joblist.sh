f5bd4c4f4388604b28ac93145976974b02a598fe
Shreyas Joshi <ssj262@gmail.com>
2018-09-12 18:07:17 -0400

    make 2 jobfiles if there are more than 300 samples

--- make_joblist.sh
+++ make_joblist.sh
@@ -3,19 +3,22 @@
 for dir in /home/sj577/project/all_sequences/*/
 	do
 		#echo $dir
-		echo "sh pipeline.sh $dir/Unaligned/*R1_001.fastq.gz $dir/Unaligned/*R2_001.fastq.gz" >> jobfile.txt
+		echo "sh pipeline.sh $dir/Unaligned/*R1*.fastq.gz $dir/Unaligned/*R2*.fastq.gz" >> jobfile.txt
 	done
-
-#In HPC 300 cores are assigned for a single run.
-#Make one job file with the first 300 lines
-
-head -300 jobfile.txt > jobfile1.txt
 
 #Count total number of lines in the jobfile
 nlines=$(wc -l < jobfile.txt)
 echo $nlines
-#Number of lines left after the first 300
-tlines=$((nlines-300))
-echo $tlines
-#Copy those lines into a new job file
-tail -$tlines jobfile.txt > jobfile2.txt
+
+#In HPC 300 cores are assigned for a single run.
+#Check if the primary jobfile has more or less than 300 lines
+if test $nlines -gt 300
+	then
+		echo "The folder thas more than 300 samples. Making a jobfile for first 300 sequences."
+		head -300 jobfile.txt > jobfile1.txt
+		#Number of lines left after the first 300
+		tlines=$((nlines-300))
+		echo "Making another jobfile for remaining $tlines samples"
+		#Copy those lines into a new job file
+		tail -$tlines jobfile.txt > jobfile2.txt
+fi
