d1e1e556f06ec4f4d2468437cafd108f0cdfcd70
Shreyas Joshi <ssj262@gmail.com>
2018-09-13 16:13:33 -0400

    accept jobfile from dsq_pipeline_run.sh

--- dSQpipeline_run.sh
+++ dSQpipeline_run.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-#SBATCH --array=0-299
-#SBATCH --job-name=jobfile1.txt
+#SBATCH --array=0-287
+#SBATCH --job-name=jobfile.txt
 #SBATCH --cpus-per-task=1
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=shreyas.joshi@yale.edu
 #SBATCH --ntasks=1
 
-/ycga-gpfs/apps/hpc/software/dSQ/0.92/dSQBatch.py $1
+/ycga-gpfs/apps/hpc/software/dSQ/0.92/dSQBatch.py jobfile.txt
